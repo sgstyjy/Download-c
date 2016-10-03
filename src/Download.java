0d41434c329a96f1bc4eed203afe5cf2dcb70e03
sgstyjy <feizhangchina@gmail.com>
2016-10-03 16:46:45 +0200

    final version

--- src/Download.java
+++ src/Download.java
@@ -10,6 +10,7 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.lang.*;
 
 public class Download {
 	 
@@ -20,40 +21,40 @@ public class Download {
 			
 			String url = args[0];
 			int MYTHREADS = Integer.parseInt(args[1]);
+			//System.out.println(MYTHREADS);
+			//System.out.println(url);
 			
 			ExecutorService executor = Executors.newFixedThreadPool(MYTHREADS);
 			Runnable worker = new MyRunnable(url);
-			executor.execute(worker);
-
+			for(int i=0; i<MYTHREADS; i++){
+				executor.execute(worker);
+			}
+			
 			executor.shutdown();
 			// Wait until all threads are finish
 			while (!executor.isTerminated()) {
-	 
 			}
 			//System.out.println("\nFinished all threads");
 		}
 	 
 		public static class MyRunnable implements Runnable {
 			private final String url;
+			private URL link;
 	 
-			MyRunnable(String url) {
+			public MyRunnable(String url) throws MalformedURLException {
 				this.url = url;
+				this.link = new URL(this.url);
 			}
 	 
 			@Override
 			public void run() {
 	 
-				//String fileName = "download/"+Thread.currentThread().getId()+".txt"; //The file that will be saved on your computer
-				 URL link = null;
-				try {
-					link = new URL(url);
-				} catch (MalformedURLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} //The file that you want to download
+				//String fileName = "~/myfile/"+Thread.currentThread().getId()+".txt"; //The file that will be saved on your computer
+				//String fileName = "/13.txt"; 
+				System.out.println(Thread.currentThread().getId()+"  "+Thread.currentThread().getName());
 				
-		     //Code to download
-				while(true){
+		        //Code to download
+				//while(true){
 					try {
 						 InputStream in = new BufferedInputStream(link.openStream());
 						 ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -76,12 +77,14 @@ public class Download {
 								e.printStackTrace();
 							}
 							fos.write(response);
+							fos.flush();
 							fos.close();*/
 						} catch (IOException e1) {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
-						}			 
-				}
+						}
+					//System.out.println(Thread.currentThread().getName()+"\n");
+				//}
 			
 			}
 		}
